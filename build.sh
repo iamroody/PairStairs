71c00e7fa17e8262fa1fcd09addf26bfb18ce9b6
Wang Yan <yanwang@thoughtworks.com>
2011-11-07 09:07:22 +0800

    add build and setup script

--- build.sh
+++ build.sh
@@ -1,1 +1,56 @@
-#!/bin/sh
+#!/bin/bash
+
+function usage {
+cat <<EOF
+ Usage: ./build.sh [COMMAND]
+ COMMAND:
+    ut:run all unit tests
+    ft:run all functional tests
+    run:start server
+    dbreset:reset database
+EOF
+}
+
+function unit_test {
+    echo "--------start to run all unit tests--------"
+    cd pairstairs
+    nosetests
+    cd ..
+}
+
+function function_test {
+    echo "--------start to run all unit tests--------"
+    cd func_test
+    nosetests -a "pairstairs"
+    cd ..
+}
+
+function run_server {
+    xterm -e "python manage.py runserver" & sleep 2
+}
+
+function db_reset {
+    echo "----------reset database-------------------"
+}
+
+function set_env {
+    set -e
+    set -u
+    DIR="$( cd "$( dirname "$0" )" && pwd )"
+    cd $DIR
+    set +u
+    source ve/bin/activate
+}
+
+function main {
+  set_env
+  case $1 in
+    ut) unit_test ;;
+    ft) function_test ;;
+    run) run_server ;;
+    dbreset) db_reset;;
+	*) usage && exit 1;;
+  esac
+}
+
+main $@
--- setup.sh
+++ setup.sh
@@ -1,1 +1,14 @@
-#!/bin/sh
+#!/bin/bash
+set -e
+set -u
+DIR="$( cd "$( dirname "$0" )" && pwd )"
+cd $DIR
+
+if [ `which python` ]; then echo "`python --version 2>&1` installed"; else echo "You have to install python by yourself" && exit 1; fi
+if [ `which pip` ]; then  echo $(echo `pip --version` | sed 's/\(.*\) from \(.*\)/\1 installed/g'); else easy_install pip; fi
+if [ `which virtualenv` ]; then echo "virtualenv `virtualenv --version` installed"; else pip install virtualenv; fi
+if [ ! -d 've' ]; then virtualenv ve; fi
+
+set +u
+source ve/bin/activate
+pip install -r requirements.pip
