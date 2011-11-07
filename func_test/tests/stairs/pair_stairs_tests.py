897c7761f2dc77bde6eaf8c8f95f83a4f90ef370
Wang Yan <yanwang@thoughtworks.com>
2011-11-07 21:59:49 +0800

    finish function test , don't create stairs when there are less 2 programmers

--- func_test/tests/stairs/pair_stairs_tests.py
+++ func_test/tests/stairs/pair_stairs_tests.py
@@ -1,10 +1,14 @@
 from nose.plugins.attrib import attr
+from selenium.webdriver.common.by import By
 from framework.basetest import BaseTest
 from framework.utils.utils import url
 
 class TestPairStairs(BaseTest):
     @attr("function_test")
     def test_should_not_create_pair_stairs_when_there_are_less_2_programmers(self):
-        self.driver.get(url('pairstairs'))
+        self.driver.get(url('/pairstairs'))
         self.assertEqual(self.driver.title, "Pair Stairs")
+        message = self.driver.find_element(By.CSS_SELECTOR,".warning_message").text
+        self.assertEqual(message, "There are not enough programmers (less than 2) to create stairs")
+
         
--- pairstairs/views.py
+++ pairstairs/views.py
@@ -4,7 +4,6 @@ from django.shortcuts import render_to_response
 def stairs(request):
     pairs = Pair.objects.all()
     programmers = Programmer.objects.all()
-
     return render_to_response('pairstairs/index.html', {
         'programmers': programmers,
         'pairs': pairs,
