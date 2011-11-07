6e99872a2f7bc58505f86024d418f8da8217a432
Wang Yan <yanwang@thoughtworks.com>
2011-11-07 17:28:44 +0800

    introduce tweet bootstrap , add urls for media dir

--- urls.py
+++ urls.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 from django.conf.urls.defaults import patterns, include, url
 
 # Uncomment the next two lines to enable the admin:
@@ -8,10 +9,9 @@ urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'PairStairs.views.home', name='home'),
     url(r'^pairstairs/', include('pairstairs.urls')),
-
     # Uncomment the admin/doc line below to enable admin documentation:
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^media/(?P<path>.*)$', 'django.views.static.serve',{'document_root': settings.MEDIA_ROOT}),
 )
